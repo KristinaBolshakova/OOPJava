1ea49dcfc9d3e731704cb03693dac0ef89a85d90
Kristina <Bolshakovakristy@gmail.com>
2023-01-24 14:53:58 +0300

    HW3

--- src/units/Archer.java
+++ src/units/Archer.java
@@ -20,18 +20,18 @@ public class Archer extends BaseHero {
 
     @Override
     public void step(ArrayList<BaseHero> heroList) {
-        boolean doShoot = true;
+        boolean stepShoot = true;
         for (BaseHero hero : heroList) {
             if (hero.type.equals("Peasant")) {
                 if (((Peasant) hero).delivery) {
                     System.out.printf("\n%s %s совершил выстрел и потратил 0 стрел. Осталось %d стрел", name, type, shoots, maxShoots);
                     ((Peasant) hero).delivery = false;
-                    doShoot = false;
+                    stepShoot = false;
                     break;
                 }
             }
         }
-        if (doShoot) {
+        if (stepShoot) {
             if (shoots > 0) {
                 System.out.printf("\n%s %s совершил выстрел и потратил 1 стрелу. Осталось %d/%d стрел", name, type, --shoots, maxShoots);
             }
